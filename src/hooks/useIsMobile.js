0df3c77be99e772d002c652307c74d5aa799e26a
nudelx <alex@equifina.com>
2024-01-07 21:47:19 +0200

    hook

---
+++ src/hooks/useIsMobile.js
@@ -0,0 +1,22 @@
+import { useCallback, useEffect, useState } from 'react'
+
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  const updateSize = useCallback(() => {
+    window.outerWidth <= 600 ? setIsMobile(true) : setIsMobile(false)
+  }, [setIsMobile])
+
+  useEffect(() => {
+    addEventListener('resize', updateSize)
+    return () => removeEventListener('resize', updateSize)
+  })
+
+  return {
+    updateSize,
+    isMobile,
+    setIsMobile,
+  }
+}
+
+export default useIsMobile
