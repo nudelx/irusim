ae3ed9b892b6f5aa45d491015d5672c014f82dd8
nudelx <alex@equifina.com>
2024-01-31 14:43:49 +0200

    new version with auto clean db

--- src/context/Data.jsx
+++ src/context/Data.jsx
@@ -1,11 +1,12 @@
 import { createContext, useMemo, useEffect, useContext, useState, useCallback } from 'react'
-import { set, onValue, ref, database, dbName } from '../db/firebase'
+import { set, onValue, ref, database, dbName, remove } from '../db/firebase'
 import PropTypes from 'prop-types'
 
 const INITIAL_STATE = Object.freeze({
   shifts: null,
   currentWeek: true,
   page: 0,
+  DELETE_THRESHOLD: 604800 * 3000, /// 3 weeks
 })
 
 export const DataContext = createContext(INITIAL_STATE)
@@ -14,6 +15,23 @@ export const useDataContext = () => useContext(DataContext)
 export const DataProvider = ({ children }) => {
   const [shifts, setShifts] = useState({})
   const [currentWeek, setCurrentWeek] = useState(true)
+
+  const deleteOldShifts = useCallback(() => {
+    const lastDateLimit = new Date().getTime() - INITIAL_STATE.DELETE_THRESHOLD
+    const shiftRefs = ref(database, `${dbName}/`)
+    onValue(shiftRefs, (snapshot) => {
+      if (snapshot.exists()) {
+        snapshot.forEach((d) => {
+          const time = d.key.replaceAll('_', '/')
+          const timestamp = new Date(time).getTime()
+          if (timestamp < lastDateLimit) {
+            console.log('to clean', time)
+            remove(d.ref)
+          }
+        })
+      }
+    })
+  }, [])
 
   const saveShift = useCallback(({ stringDate, shift }) => {
     set(ref(database, `${dbName}/` + stringDate.replaceAll('/', '_')), {
@@ -38,6 +56,10 @@ export const DataProvider = ({ children }) => {
     })
   }, [])
 
+  useEffect(() => {
+    deleteOldShifts()
+  }, [deleteOldShifts])
+
   const value = useMemo(
     () => ({
       shifts,
@@ -45,8 +67,9 @@ export const DataProvider = ({ children }) => {
       setShifts,
       setCurrentWeek,
       saveShift,
+      deleteOldShifts,
     }),
-    [shifts, currentWeek, setCurrentWeek, setShifts, saveShift],
+    [shifts, currentWeek, setCurrentWeek, setShifts, saveShift, deleteOldShifts],
   )
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>
